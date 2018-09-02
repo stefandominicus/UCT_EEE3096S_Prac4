c4fdf823d1c41141d1ce48906dfb0f81c5405457
stefandominicus <stefandominicus@gmail.com>
2018-09-02 20:10:27 +0200

    Updated py file

--- prac4.py
+++ prac4.py
@@ -5,16 +5,16 @@
 
 #!/usr/bin/python3
 
-
 import RPi.GPIO as GPIO
 import Adafruit_MCP3008
 import time
 import os
 
 ###---SETUP---###
-# Pins
+
 GPIO.setmode(GPIO.BCM)
 
+# Buttons
 resetPin = 26
 frequencyPin = 19
 stopPin = 13
@@ -25,6 +25,7 @@ GPIO.setup(frequencyPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(stopPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(displayPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# SPI - ADC
 SPICLK = 11
 SPIMISO = 9
 SPIMOSI = 10
@@ -35,12 +36,9 @@ GPIO.setup(SPIMISO, GPIO.IN)
 GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
 
-mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
+mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO) #ADC object
 
-# global variable
-values = [0]*8
-
-
+values = [0]*8 #data from ADC
 
 # Interrupt Methods
 def resetPush(channel):
@@ -62,14 +60,16 @@ def displayPush(channel):
 	if (GPIO.input(channel) == GPIO.LOW): #avoid trigger on button realease
 		os.system('clear')
 		print("Display button pushed")
+		print(mcp.read_adc(1))
 
-# Interrupt Setup
+# Interrupt Event Detection
 GPIO.add_event_detect(resetPin, GPIO.FALLING, callback=resetPush, bouncetime=100)
 GPIO.add_event_detect(frequencyPin, GPIO.FALLING, callback=frequencyPush, bouncetime=100)
 GPIO.add_event_detect(stopPin, GPIO.FALLING, callback=stopPush, bouncetime=100)
 GPIO.add_event_detect(displayPin, GPIO.FALLING, callback=displayPush, bouncetime=100)
 
 
+###---LOOP---###
 try:
 	print("Ready!")
 	#keep the program running, waiting for button presses
